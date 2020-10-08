bbe57c4cb1c739a64ce3172dda25f80d29586278
ashtron <ashton.playsted@protonmail.com>
2020-10-08 13:23:07 +0200

    Can add new items

--- js/main.js
+++ js/main.js
@@ -1,23 +1,52 @@
 window.onload = () => {
     const airtableApiKey = config.AIRTABLE_API_KEY;
     const shoppingList = document.getElementById("shopping-list");
+    const itemInput = document.getElementById("item-input");
+    const saveButton = document.getElementById("save-button");
+
+    saveButton.addEventListener("click", addItem);
 
     getItems();
     
     function getItems() {
         axios.get(`https://api.airtable.com/v0/appW7LfolPCsDiVXX/Table%201?api_key=${airtableApiKey}`)
-        .then(response => {
-            response.data.records.forEach(item => {
-                let newShoppingListItem = document.createElement("li");
-                newShoppingListItem.innerText = item.fields.item;
-                newShoppingListItem.classList.add("shopping-list-item");
-                newShoppingListItem.addEventListener("click", (event) => {
-                    event.target.classList.toggle("strikethrough");
+            .then(response => {
+                response.data.records.forEach(item => {
+                    let newShoppingListItem = createShoppingListItem(item.fields.item);
+
+                    shoppingList.append(newShoppingListItem);
                 });
-                shoppingList.append(newShoppingListItem);
             });
-        });
     }
 
-    function removeItem() {}
+    function removeItem(event) {
+        console.log(event.target.parentNode.innerText);
+    }
+
+    function addItem(event) {
+        const newItem = itemInput.value;
+
+        axios.post(`https://api.airtable.com/v0/appW7LfolPCsDiVXX/Table%201?api_key=${airtableApiKey}`, {
+                "fields": {
+                    "item": newItem
+                }
+            })
+            .then(response => {
+                shoppingList.innerHTML = "";
+                getItems();
+            });
+    }
+
+    function createShoppingListItem(item) {
+        const newItem = document.createElement("li");
+
+        newItem.classList.add("shopping-list-item");
+        newItem.innerText = item;
+
+        newItem.addEventListener("click", (event) => {
+            event.target.classList.toggle("strikethrough");
+        });
+
+        return newItem;
+    }
 }
